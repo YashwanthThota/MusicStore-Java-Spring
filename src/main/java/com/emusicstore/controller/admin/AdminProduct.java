d6d06e42a0c57fe2956c92e8a5697512aa6205f8
Yashwanth Thota <31579270+YashwanthThota@users.noreply.github.com>
2018-10-04 21:40:46 -0500

    heroku

--- src/main/java/com/emusicstore/controller/admin/AdminProduct.java
+++ src/main/java/com/emusicstore/controller/admin/AdminProduct.java
@@ -54,7 +54,7 @@ public class AdminProduct {
 
         MultipartFile productImage = product.getProductImage();
         String rootDirectory = request.getSession().getServletContext().getRealPath("/");
-        path = Paths.get(rootDirectory + "\\WEB-INF\\resources\\images\\" + product.getProductId()+".png");
+        path = Paths.get(rootDirectory + "//WEB-INF//resources//image//" + product.getProductId()+".png");
 
         if(productImage != null && !productImage.isEmpty()){
             try {
